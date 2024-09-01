0f0fe5b8c371ccdacae259fd926564ed99beea63
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-09-01 08:51:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,29 @@
-from app import db
-from datetime import datetime
+# models.py
+from extensions import db
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 class Quiz(db.Model):
+    __tablename__ = 'quizzes'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    questions = db.relationship('Question', backref='quiz', lazy=True)
+
+    def __repr__(self):
+        return f'<Quiz {self.title}>'
 
 class Question(db.Model):
+    __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key=True)
-    question_text = db.Column(db.String(200), nullable=False)
-    answer = db.Column(db.String(100), nullable=False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    quiz = db.relationship('Quiz', backref=db.backref('questions', lazy=True))
+
+    def __repr__(self):
+        return f'<Question {self.text[:20]}>'
