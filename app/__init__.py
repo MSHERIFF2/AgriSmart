2e126b02aa9e351bcffbe39c5e2f12d1b227de62
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-09-02 17:43:16 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from .routes import main_routes
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.config')
 
-    app.register_blueprint(main_routes)
+   db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
+
+    app.register_blueprint(main_routes)
     return app
