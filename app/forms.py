2012c4760dcfaa0b68788e4eb96a76fc7c1fb8e4
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-09-03 14:45:06 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -13,3 +13,13 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Register')
+
+class AdminRegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password', message='Passwords must match.')
+    ])
+    admin_key = StringField('Admin Key', validators=[DataRequired()])  # Optional, for admin verification
+    submit = SubmitField('Register Admin')
+
