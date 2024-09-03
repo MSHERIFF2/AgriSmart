1b7de17f48c820ae5973b68c6b391cdb3a3dbba3
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-09-03 14:00:43 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
