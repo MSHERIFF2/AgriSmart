b50ccc8cbabe080381df5e17dc461ef5f2c07334
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-09-11 18:29:40 +0100

    update

--- app/routes/weather_forecast.py
+++ app/routes/weather_forecast.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, request, jsonify
 from app.models import WeatherForecast
 from app import db
+from datetime import date
+
+query_date = date(2024, 9, 11)
 
 weather_forecast = Blueprint('weather_forecast', __name__)
 
 @weather_forecast.route('/weather_forecast', methods=['GET'])
 def get_weather_forecast():
-    forecast = WeatherForecast.query.filter_by(date=date).first()
+    forecast = WeatherForecast.query.filter_by(date=query_date).first()
     if forecast:
         return jsonify(forecast.to_dict())
     else:
