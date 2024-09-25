7e2fb8a2005e0ecd285004e3fca8c1230cc0b11d
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2024-09-25 09:10:06 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-
+''' This is an entry point to the project'''
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
